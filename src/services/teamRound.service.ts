b78512bfafd65ae1c3d0b4c91df6f710796f664f
lqhuy28 <lequanghuy2809@gmail.com>
2025-04-03 12:39:45 +0700

    update teamRound service

--- src/services/teamRound.service.ts
+++ src/services/teamRound.service.ts
@@ -14,6 +14,24 @@ class TeamRoundService {
       throw error;
     }
   }
+
+  async getTeamRoundsByJudgeAndRound(judgeId: string, roundId: string): Promise<TeamRound[]> {
+    try {
+      const response = await apiService.auth.post<TeamRound[]>(
+        "/api/v1/team-rounds/filter-by-judge-and-round",
+        {
+          data: {
+            judgeId: judgeId,
+            roundId: roundId
+          }
+        }
+      );
+      return response.data;
+    } catch (error) {
+      console.error("Error fetching teamRounds by judgeId and roundId:", error);
+      throw error;
+    }
+  }
 }
 
 export const teamRoundService = new TeamRoundService();
